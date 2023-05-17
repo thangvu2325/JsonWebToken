84582c206147c475ea55e29bed3e2c0acb8bfd26
thangvu2325 <99258025+thangvu2325@users.noreply.github.com>
2023-05-17 08:47:38 +0700

    ab

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const {engine} = require('express-handlebars');
 const cookieParser = require("cookie-parser");
-// const mongoose = require('mongoose');
 const db = require('./config/db')
 const cors = require("cors");
 
@@ -18,13 +17,9 @@ app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
 app.use(cors());
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true
-// }));
+;
 const route = require('./routes');
 
-// const { connect } = require('http2');
 
 app.use(express.static(path.join(__dirname,'public')))
 
